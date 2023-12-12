6bfdcb4e12cce82d319d7c960c1205612e06bd97
Intrum1 <skoropadskiy1979@gmail.com>
2023-12-12 18:54:26 +0100

    1

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,21 +1,26 @@
-import styles from './ContactList.module.css'
-import React from "react";
+import styles from './ContactList.module.css';
+import React from 'react';
+// Імпортуємо хук
+import { useSelector } from 'react-redux';
 
 const ContactList = ({ contacts, onDeleteContact }) => {
-
-    return (
-      <ul className={styles.contactList}>
-        {contacts.map((contact) => (
-          <li key={contact.id} className={styles.contactItem}>
-            {contact.name}: {contact.number}
-            <button onClick={() => onDeleteContact(contact.id)} className={styles.button}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-
+  const contact = useSelector(state => state.contacts.status);
+  console.log(contact);
+  return (
+    <ul className={styles.contactList}>
+      {contacts.map(contact => (
+        <li key={contact.id} className={styles.contactItem}>
+          {contact.name}: {contact.number}
+          <button
+            onClick={() => onDeleteContact(contact.id)}
+            className={styles.button}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default ContactList;
